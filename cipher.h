dd8b6e612967843bff7bfbbc24cbf3a79c5a140a
lksenya <145290315+lksenya@users.noreply.github.com>
2023-12-21 17:23:45 +0300

    Create cipher.h

---
+++ cipher.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <algorithm>
+class Cipher
+{
+
+public:
+    Cipher(int key) : key_(key) {}
+    std::string encrypt(std::string text);
+    std::string decrypt(std::string text);
+private:
+    int key_;
+};
+class cipher_error : public std::invalid_argument
+{
+public:
+    explicit cipher_error(const std::string& what_arg) :
+        std::invalid_argument(what_arg) {}
+    explicit cipher_error(const char* what_arg) :
+        std::invalid_argument(what_arg) {}
+};
